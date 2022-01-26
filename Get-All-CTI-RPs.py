eec5d792254bd010e1810487b70c82d5a336a619
thayanc1 <91150199+thayanc1@users.noreply.github.com>
2022-01-26 14:52:12 -0500

    Add files via upload

---
+++ Get-All-CTI-RPs.py
@@ -0,0 +1,8 @@
+# From Login.py module get the Login Credential
+from Login import cucm_username, cucm_password, cucm, version
+from ciscoaxl import axl
+
+ucm = axl(username=cucm_username,password=cucm_password,cucm=cucm,cucm_version=version)
+
+for cti in ucm.get_cti_route_points():
+    print(cti.name)
