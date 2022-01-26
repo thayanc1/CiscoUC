76f428da064d5aeae66f8fb6a8b88ddcf4c2cb9e
thayanc1 <91150199+thayanc1@users.noreply.github.com>
2022-01-26 14:07:56 -0500

    Delete Get-All-DNs.py

--- Get-All-DNs.py
+++
@@ -1,7 +0,0 @@
-from Login import cucm_username, cucm_password, cucm, version
-from ciscoaxl import axl
-
-ucm = axl(username=cucm_username,password=cucm_password,cucm=cucm,cucm_version=version)
-
-for dn in ucm.get_directory_numbers():
-    print(dn.uuid)
