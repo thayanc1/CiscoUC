6ddad5a2d5a04e05f80037ccef298f0f968aafe8
thayanc1 <91150199+thayanc1@users.noreply.github.com>
2022-01-26 14:55:53 -0500

    Add files via upload

---
+++ ATSG-DP-for-Devices-CSV.py
@@ -0,0 +1,39 @@
+# Library to send HTTP Requests:
+import requests
+# Library to read XML Documents:
+import xml.etree.ElementTree as ET
+# Library to read CSV Documents:
+import csv
+
+# To disble the Certificate SubjectAlternateName Warning
+requests.packages.urllib3.disable_warnings()
+
+# To Trust the CUCM Tomcat Certificate
+TomcatCertLocation = 'C:/Users/ATSGAdmin/Documents/ATSG/tomcat.pem'
+
+# Open the CSV file contains the Device Name list if the Devices
+with open('C:/Users/ATSGAdmin/Documents/ATSG/Phone-List.csv', 'r') as csv_file:
+    inputDevNames = csv.reader(csv_file)
+    for DevName in inputDevNames:
+        # Capture the Device Name from the CSV File Location "0"
+        Device = str(DevName[0])
+        
+        # From SoapUI getPhone Code <<Copy and Paste from SoapUI>>
+        # Two Sections """ """ + Device + """ """
+        soapReq = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns="http://www.cisco.com/AXL/API/12.5">
+        <soapenv:Header/>
+        <soapenv:Body>
+            <ns:getPhone>
+            <name>""" + Device + """</name>
+            </ns:getPhone>
+        </soapenv:Body>
+        </soapenv:Envelope>"""
+
+        # Send HTTP Post request to CUCM with AXL API User 
+        r = requests.post('https://RC-UCM-Pub.atsg.net/axl/',verify=TomcatCertLocation,auth=('tnagarajah','Tnagara!ah2$'),data=soapReq)
+
+        # Write to XML File if the r.contetn
+        XMLDoc = ET.fromstring(r.content)
+        #print(r.content)
+        # Serach for Device Pool Info and Print Device Name, Device Pool for the CSV Device List
+        print(Device+"," + XMLDoc.find(".//devicePoolName").text)
