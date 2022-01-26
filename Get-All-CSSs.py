fdb67e524c8fa3851d9e445834f64d269b15de88
thayanc1 <91150199+thayanc1@users.noreply.github.com>
2022-01-26 14:07:07 -0500

    Delete Get-All-CSSs.py

--- Get-All-CSSs.py
+++
@@ -1,7 +0,0 @@
-from Login import cucm_username, cucm_password, cucm, version
-from ciscoaxl import axl
-
-ucm = axl(username=cucm_username,password=cucm_password,cucm=cucm,cucm_version=version)
-
-for css in ucm.get_calling_search_spaces():
-    print(css.name)
