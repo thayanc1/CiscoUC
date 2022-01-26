a38657965f7ec33b1e24ade796a9bb9110002f26
thayanc1 <91150199+thayanc1@users.noreply.github.com>
2022-01-26 14:08:52 -0500

    Delete Get-All-Translation-Patterns.py

--- Get-All-Translation-Patterns.py
+++
@@ -1,8 +0,0 @@
-from Login import cucm_username, cucm_password, cucm, version
-from ciscoaxl import axl
-
-ucm = axl(username=cucm_username,password=cucm_password,cucm=cucm,cucm_version=version)
-
-for trans in ucm.get_translations():
-    detailed = ucm.get_translation(uuid=trans.uuid)
-    print(detailed.description)
