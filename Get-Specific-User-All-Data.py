9f2d68564635d306958c04a37436e5210f2170f4
thayanc1 <91150199+thayanc1@users.noreply.github.com>
2022-01-26 14:09:06 -0500

    Delete Get-Specific-User-All-Data.py

--- Get-Specific-User-All-Data.py
+++
@@ -1,8 +0,0 @@
-from Login import cucm_username, cucm_password, cucm, version
-from ciscoaxl import axl
-
-ucm = axl(username=cucm_username,password=cucm_password,cucm=cucm,cucm_version=version)
-user_id=input('Enter UserID: ')
-
-user = ucm.get_user(user_id)
-print(user)
