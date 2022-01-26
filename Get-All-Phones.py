6ee0f1f045c5b35a51a387708c9ca47f98bf1f67
thayanc1 <91150199+thayanc1@users.noreply.github.com>
2022-01-26 14:08:33 -0500

    Delete Get-All-Phones.py

--- Get-All-Phones.py
+++
@@ -1,7 +0,0 @@
-from Login import cucm_username, cucm_password, cucm, version
-from ciscoaxl import axl
-
-ucm = axl(username=cucm_username,password=cucm_password,cucm=cucm,cucm_version=version)
-
-for phone in ucm.get_phones():
-    print(phone.name)
