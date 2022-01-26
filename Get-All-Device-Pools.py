184cfab57dcdd7574aac8f675eb43c3e15712fa5
thayanc1 <91150199+thayanc1@users.noreply.github.com>
2022-01-26 14:48:39 -0500

    Add files via upload

---
+++ Get-All-Device-Pools.py
@@ -0,0 +1,8 @@
+# From Login.py module get the Login Credential
+from Login import cucm_username, cucm_password, cucm, version
+from ciscoaxl import axl
+
+ucm = axl(username=cucm_username,password=cucm_password,cucm=cucm,cucm_version=version)
+
+for dp in ucm.get_device_pools():
+    print(dp.name)
